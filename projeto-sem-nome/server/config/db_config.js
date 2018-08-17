5d415d489dbccfcfeeb92434aa21d9a4e8f97ed3
jslucassf <jslucassf@gmail.com>
2018-08-17 19:50:29 -0300

    Adiciona senha padrão para banco de dados local

--- projeto-sem-nome/server/config/db_config.js
+++ projeto-sem-nome/server/config/db_config.js
@@ -4,7 +4,7 @@ var pool = mysql.createPool({
     host     : 'localhost',
     user     : 'root',
     port     : '3306',
-    password : '',
+    password : 'hackfest',
     database : 'hackfest_db',
     charset: 'latin1'
 })
