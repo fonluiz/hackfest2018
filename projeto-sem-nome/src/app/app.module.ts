56d6b9ac8ef2824571496e92ffb6971a419b24b2
fonluiz <luiz.fonseca@ccc.ufcg.edu.br>
2018-08-17 13:57:12 -0300

    Adiciona bootstrap à aplicação

--- projeto-sem-nome/src/app/app.module.ts
+++ projeto-sem-nome/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
