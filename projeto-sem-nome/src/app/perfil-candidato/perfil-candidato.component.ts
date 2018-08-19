bc7b45ebbc5aa1b5b0148c880e4cf888c9c94730
Hugo Gabriel <hugo.silva@ccc.ufcg.edu.br>
2018-08-19 03:54:50 -0300

    adiciona número de proposições

--- projeto-sem-nome/src/app/perfil-candidato/perfil-candidato.component.ts
+++ projeto-sem-nome/src/app/perfil-candidato/perfil-candidato.component.ts
@@ -1,32 +1,44 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { CandidatesService } from '../services/candidates.service';
-import { Router, ActivatedRoute } from '../../../node_modules/@angular/router';
-import { Location } from '../../../node_modules/@angular/common';
+import { Component, OnInit, Input } from "@angular/core";
+import { CandidatesService } from "../services/candidates.service";
+import { Router, ActivatedRoute } from "../../../node_modules/@angular/router";
+import { Location } from "../../../node_modules/@angular/common";
 
 @Component({
-  selector: 'app-perfil-candidato',
-  templateUrl: './perfil-candidato.component.html',
-  styleUrls: ['./perfil-candidato.component.scss']
+  selector: "app-perfil-candidato",
+  templateUrl: "./perfil-candidato.component.html",
+  styleUrls: ["./perfil-candidato.component.scss"]
 })
 export class PerfilCandidatoComponent implements OnInit {
-
   candidateId: string;
   candidate: any;
+  numProposicoes: any;
 
   constructor(
     private candidatesService: CandidatesService,
     private route: ActivatedRoute
-  ) { }
+  ) {}
 
   ngOnInit() {
     this.route.params.subscribe(params => {
-      this.candidateId = params['id'];
-      console.dir(this.candidateId);
-      this.candidatesService.getCandidate(this.candidateId).subscribe(
-        cand => this.candidate = cand[0]
-      );
-    })
-    
+      this.candidateId = params["id"];
+      this.candidatesService.getCandidate(this.candidateId).subscribe(cand => {
+        this.candidate = cand[0];
+        this.calcula_proposicoes(this.candidate.Nome_Completo);
+      });
+    });
   }
 
+  calcula_proposicoes(nome: string) {
+    // let nome = "MARIA LAURA MONTEZA DE SOUZA CARNEIRO";
+    this.route.params.subscribe(params => {
+      this.candidatesService.getNumProposicoes(nome).subscribe(prop => {
+        let propArray = <Array<any>>prop;
+        if (propArray.length === 0) {
+          this.numProposicoes = 0;
+        } else {
+          this.numProposicoes = prop[0].numero_proposicoes;
+        }
+      });
+    });
+  }
 }
