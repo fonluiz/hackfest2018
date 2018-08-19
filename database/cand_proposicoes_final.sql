e8af0ba863993f51b1af610d154da94eb01736b5
Gileade Kelvin <gileadekelvin@gmail.com>
2018-08-18 21:13:52 -0300

    Edita cand_proposicoes_final

--- database/cand_proposicoes_final.sql
+++ database/cand_proposicoes_final.sql
@@ -3,8 +3,8 @@ USE hackfest_db;
 DROP TABLE IF EXISTS cand_proposicoes_final;
 CREATE TABLE cand_proposicoes_final(
   id_autor VARCHAR(8),
-  n VARCHAR(3),
-  candidato_nome VARCHAR(50)
+  numero_proposicoes VARCHAR(3),
+  candidato_nome VARCHAR(50),
   
   CONSTRAINT cand_proposicoes_final_pk PRIMARY KEY(id_autor)
 );
