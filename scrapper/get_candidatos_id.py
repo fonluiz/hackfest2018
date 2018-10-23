3377dfcd357aaa30af8e6e4b00ab08317d209d7e
Júlio César Neves <juliocnevess@gmail.com>
2018-10-23 10:56:34 -0300

    Atualiza scrapper para utilizar python 3

--- scrapper/get_candidatos_id.py
+++ scrapper/get_candidatos_id.py
@@ -1,12 +1,9 @@
 # -*- coding: utf-8 -*-
+
 import json
-import sys
-import urllib2
+import requests
 
-reload(sys)
-sys.setdefaultencoding('utf8')
-
-states = ["AC","AL","AP",'AM','BA', 'CE','DF','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO']
+states = ['AC','AL','AP','AM','BA','CE','DF','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO']
 selected_role = "deputado_federal"
 roles = {}
 roles['governador'] = 3
@@ -20,20 +17,17 @@ roles['suplente2'] = 10
 
 text = '"id";"nome";"estado"\n'
 for state in states:
-	print state
+	print(state)
 	url = 'http://divulgacandcontas.tse.jus.br/divulga/rest/v1/candidatura/listar/2018/' + state + '/2022802018/' + str(roles[selected_role]) + '/candidatos'
-	req = urllib2.Request(url)
 
-	response = urllib2.urlopen(req)
+	response = requests.get(url)
 
-	candidatos = response.read()
-
-	data = json.loads(candidatos)
+	data = json.loads(response.content.decode('utf-8'))
 	for candidato in data["candidatos"]:
 		text +=  '"' + str(candidato["id"]) + '";'
 		text += '"' + candidato["nomeCompleto"] + '";'
 		text += '"' + state + '"\n'
 
-	arq = open("id_nome_todos_estados_" + selected_role ,"w")
+	arq = open('id_nome_todos_estados_' + selected_role + '.csv' ,'w')
 	arq.write(text)
 	arq.close()
